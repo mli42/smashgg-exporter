b693742d952941f01e411709b5d558135cbdc418
mli42 <mli@student.42.fr>
2025-09-20 00:55:34 +0200

    wip(query): tournament query

---
+++ queries/tournaments.query.py
@@ -0,0 +1,33 @@
+TOURNAMENTS_QUERY = """
+query TournamentsQuery($perPage: Int, $coordinates: String!, $radius: String!) {
+  tournaments(
+    query: {sortBy: "startAt desc", perPage: $perPage, filter: {location: {distanceFrom: $coordinates, distance: $radius}, past: true, videogameIds: [1386], countryCode: "FR", addrState: "IDF", afterDate: 1735689600, beforeDate: 1743465600}}
+  ) {
+    pageInfo {
+      total
+      totalPages
+      page
+      perPage
+    }
+    nodes {
+      id
+      name
+      events(filter: {videogameId: [1386]}) {
+        name
+        videogame {
+          id
+          name
+        }
+      }
+    }
+  }
+}
+"""
+
+"""
+{
+  "perPage": 5,
+  "coordinates": "48.853495,2.348391",
+  "radius": "5mi"
+}
+"""
