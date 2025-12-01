eccce30a3c6345ef3ba6bde322d018fe264e92cf
mli42 <mli@student.42.fr>
2025-12-01 15:46:21 +0100

    chore: move getTournaments function

--- src/main.py
+++ src/main.py
@@ -1,55 +1,12 @@
 import asyncio
-import os
-from time import sleep
-from typing import cast
 
-import requests
 from dotenv import load_dotenv
 
-from customTypes.startgg import StartggResponse, SuccessResponse
-from queries.tournamentsQuery import TOURNAMENTS_QUERY, VARIABLES
-
-
-def get_tournaments(url, json, headers) -> SuccessResponse:
-    retries = 3
-
-    for attempt in range(retries):
-        if attempt != 0:
-            sleep(3)
-
-        try:
-            response: StartggResponse = requests \
-                .post(url, json=json, headers=headers) \
-                .json()
-
-            if (response.get('success') is False):
-                raise Exception(response.get('message'))
-
-            return cast(SuccessResponse, response)
-        except Exception as e:
-            print(f"Error with request. Retrying ({attempt+1}/{retries})...")
-            print(f"> {e}")
-
-    raise RuntimeError("Request failed after retries")
+from queries.getTournaments import get_tournaments
 
 
 async def main():
-    STARTGG_TOKEN = os.getenv('STARTGG_TOKEN')
-
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f"Bearer {STARTGG_TOKEN}",
-    }
-
-    url = "https://api.start.gg/gql/alpha"
-
-    response = get_tournaments(
-        url,
-        json={
-            "query": TOURNAMENTS_QUERY,
-            "variables": VARIABLES
-        },
-        headers=headers)
+    response = get_tournaments()
 
     queryComplexity = response['extensions']['queryComplexity']
     pageInfo = response['data']['tournaments']['pageInfo']
---
+++ src/queries/getTournaments.py
@@ -0,0 +1,46 @@
+import os
+from time import sleep
+from typing import cast
+
+import requests
+
+from customTypes.startgg import StartggResponse, SuccessResponse
+
+from .tournamentsQuery import TOURNAMENTS_QUERY, VARIABLES
+
+STARTGG_URL = "https://api.start.gg/gql/alpha"
+
+
+def get_tournaments() -> SuccessResponse:
+    STARTGG_TOKEN = os.getenv('STARTGG_TOKEN')
+
+    HEADERS = {
+        'Content-Type': 'application/json',
+        'Authorization': f"Bearer {STARTGG_TOKEN}",
+    }
+
+    BODY = {
+        "query": TOURNAMENTS_QUERY,
+        "variables": VARIABLES
+    }
+
+    retries = 3
+
+    for attempt in range(retries):
+        if attempt != 0:
+            sleep(3)
+
+        try:
+            response: StartggResponse = requests \
+                .post(STARTGG_URL, json=BODY, headers=HEADERS) \
+                .json()
+
+            if (response.get('success') is False):
+                raise Exception(response.get('message'))
+
+            return cast(SuccessResponse, response)
+        except Exception as e:
+            print(f"Error with request. Retrying ({attempt+1}/{retries})...")
+            print(f"> {e}")
+
+    raise RuntimeError("Request failed after retries")
