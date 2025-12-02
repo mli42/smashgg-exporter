f5eb6aed474e1d88b7a43bfbfe6f2e0e816e4026
mli42 <mli@student.42.fr>
2025-12-02 15:52:35 +0100

    feat: iterate over events + skip blacklisted patterns

--- src/main.py
+++ src/main.py
@@ -2,17 +2,29 @@ import asyncio
 
 from dotenv import load_dotenv
 
+from customTypes.startgg import Tournament
 from getDateTimestamp import get_date_timestamp
 from queries.getTournaments import get_tournaments_iter
+from utils.skipEvent import skipEvent
+
+
+def handle_tournament(tournament: Tournament):
+    for event in tournament['events']:
+        event_slug = event['slug'].split('/')[-1]
+
+        if skipEvent(event):
+            # print(f"SKIPPING EVENT : {event_slug} ({event['id']})")
+            continue
+
+        print(event_slug)
 
 
 async def main():
     afterDate = get_date_timestamp("01/01/2025")
     beforeDate = get_date_timestamp("01/04/2025")
 
-    tournaments = []
     for tournament in get_tournaments_iter(afterDate, beforeDate):
-        tournaments.append(tournament)
+        handle_tournament(tournament)
 
 
 if __name__ == '__main__':
---
+++ src/utils/skipEvent.py
@@ -0,0 +1,25 @@
+import re
+
+from customTypes.startgg import Event
+
+BLACKLISTED_PATTERNS = [
+    r'side-event',
+    r'attente',
+    r'melt-chill',
+    r'double',
+    r'cpu',
+    r'random',
+    r'squadstrike',
+    r'squad-strike',
+    r'squads',
+    r'amiibo',
+]
+
+
+def skipEvent(event: Event) -> bool:
+    event_slug = event['slug'].split('/')[-1]
+
+    return (
+        event['state'] != 'COMPLETED' or
+        any(re.search(pattern, event_slug) for pattern in BLACKLISTED_PATTERNS)
+    )
